36e03e88e4df2ad12bf91dcdba9c8e3f51493c9f
Sebastian Riedel <sri@cpan.org>
2014-03-29 02:22:46 +0100

    bump version

--- lib/Minion.pm
+++ lib/Minion.pm
@@ -7,7 +7,7 @@ use Minion::Job;
 use Minion::Worker;
 use Mojo::Server;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 has app  => sub { Mojo::Server->new->build_app('Mojo::HelloWorld') };
 has jobs => sub { $_[0]->mango->db->collection($_[0]->prefix . '.jobs') };
