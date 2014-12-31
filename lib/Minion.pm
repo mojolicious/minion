21dd0ade08270f353d6553d91bc9de411ab5e7ae
Sebastian Riedel <sri@cpan.org>
2014-12-31 17:06:01 +0100

    happy new year

--- lib/Minion.pm
+++ lib/Minion.pm
@@ -308,7 +308,7 @@ Brian Medley
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2014, Sebastian Riedel.
+Copyright (C) 2014-2015, Sebastian Riedel.
 
 This program is free software, you can redistribute it and/or modify it under
 the terms of the Artistic License version 2.0.
