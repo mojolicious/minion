c98e453e343698b1e9f24bee2e32a1c3317f6680
Sebastian Riedel <sri@cpan.org>
2015-02-28 09:15:50 +0100

    use current_timestamp correctly

--- lib/Minion/Backend/Pg.pm
+++ lib/Minion/Backend/Pg.pm
@@ -30,9 +30,9 @@ sub enqueue {
   my $db = $self->pg->db;
   return $db->query(
     "insert into minion_jobs
-       (args, created, delayed, priority, retries, state, task)
+       (args, delayed, priority, retries, state, task)
      values
-       (?, now(), (now() + (interval '1 second' * ?)), ?, ?, ?, ?)
+       (?, (now() + (interval '1 second' * ?)), ?, ?, ?, ?)
      returning id", {json => $args}, $options->{delay} // 0,
     $options->{priority} // 0, 0, 'inactive', $task
   )->hash->{id};
@@ -89,10 +89,8 @@ sub register_worker {
     = 'update minion_workers set notified = now() where id = ? returning 1';
   return $id if $id && $self->pg->db->query($sql, $id)->rows;
 
-  return $self->pg->db->query(
-    'insert into minion_workers (host, notified, pid, started)
-     values (?, now(), ?, now()) returning id', hostname, $$
-  )->hash->{id};
+  $sql = 'insert into minion_workers (host, pid) values (?, ?) returning id';
+  return $self->pg->db->query($sql, hostname, $$)->hash->{id};
 }
 
 sub remove_job {
@@ -446,5 +444,8 @@ drop function if exists minion_jobs_insert_notify();
 drop table if exists minion_workers;
 
 -- 2 up
+alter table minion_jobs alter column created set default current_timestamp;
 alter table minion_workers
-  add column notified timestamp with time zone not null;
+  add column
+  notified timestamp with time zone not null default current_timestamp;
+alter table minion_workers alter column started set default current_timestamp;
