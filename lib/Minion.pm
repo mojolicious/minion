fd995d7441e83c248219f595caa5b2e2aa3e89d1
Sebastian Riedel <sri@cpan.org>
2015-11-09 12:33:01 +0100

    use an easier to understand example

--- lib/Minion.pm
+++ lib/Minion.pm
@@ -233,7 +233,8 @@ Backend, usually a L<Minion::Backend::Pg> object.
   $minion = $minion->backoff(sub {...});
 
 A callback used to calculate the delay for automatically retried jobs, defaults
-to C<(retries ** 4) + 15> (15, 16, 31, 96, 271, 640...).
+to C<(retries ** 4) + 15> (15, 16, 31, 96, 271, 640...), which means that
+roughly C<25> attempts will be made in C<21> days.
 
   $minion->backoff(sub {
     my $retries = shift;
