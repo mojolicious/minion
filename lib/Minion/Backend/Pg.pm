87d881d6aa1ade24d6442e2f2e1787cd143d730e
Sebastian Riedel <sri@cpan.org>
2016-01-22 22:31:44 +0100

    wait for Travis with PostgreSQL 9.5 features

--- lib/Minion/Backend/Pg.pm
+++ lib/Minion/Backend/Pg.pm
@@ -194,7 +194,7 @@ sub _try {
          and task = any (?)
        order by priority desc, created
        limit 1
-       for update skip locked
+       for update
      )
      returning id, args, retries, task", $id,
     $options->{queues} || ['default'], [keys %{$self->minion->tasks}]
