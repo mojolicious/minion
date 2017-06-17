5f6f7a583c57539b475e13a1972dfa6a9b4a9daf
Sebastian Riedel <sri@cpan.org>
2017-06-17 18:43:00 +0200

    just call it rate limiting

--- lib/Minion.pm
+++ lib/Minion.pm
@@ -128,9 +128,9 @@ Minion - Job queue
 
 L<Minion> is a job queue for the L<Mojolicious|http://mojolicious.org> real-time
 web framework, with support for multiple named queues, priorities, delayed jobs,
-job dependencies, job results, retries with backoff, statistics, distributed
-workers, parallel processing, autoscaling, remote control, resource leak
-protection and multiple backends (such as
+job dependencies, job results, retries with backoff, rate limiting, statistics,
+istributed workers, parallel processing, autoscaling, remote control, resource
+leak protection and multiple backends (such as
 L<PostgreSQL|http://www.postgresql.org>).
 
 Job queues allow you to process time and/or computationally intensive tasks in
