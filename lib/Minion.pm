54621520891ff8ab7b498e3e02873d41065dfe7d
Sebastian Riedel <sri@cpan.org>
2018-04-13 19:47:29 +0200

    next release will be 9.0

--- lib/Minion.pm
+++ lib/Minion.pm
@@ -18,7 +18,7 @@ has missing_after => 1800;
 has remove_after  => 172800;
 has tasks         => sub { {} };
 
-our $VERSION = '8.13';
+our $VERSION = '9.0';
 
 sub add_task { ($_[0]->tasks->{$_[1]} = $_[2]) and return $_[0] }
 
