c8a7f9ac846cfa63b5f80be1cd83b3414c2ebadf
Sebastian Riedel <sri@cpan.org>
2020-01-31 00:36:17 +0100

    Bump version

--- lib/Minion.pm
+++ lib/Minion.pm
@@ -23,7 +23,7 @@ has missing_after => 1800;
 has remove_after  => 172800;
 has tasks         => sub { {} };
 
-our $VERSION = '10.04';
+our $VERSION = '10.05';
 
 sub add_task { ($_[0]->tasks->{$_[1]} = $_[2]) and return $_[0] }
 
