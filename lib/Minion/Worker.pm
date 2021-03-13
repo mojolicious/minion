964bed43c376228a905a1c6faa10f5dea78eb129
Sebastian Riedel <sri@cpan.org>
2021-03-13 16:30:59 +0100

    Add an example for how to use the run method

--- lib/Minion/Worker.pm
+++ lib/Minion/Worker.pm
@@ -373,6 +373,11 @@ Register worker or send heartbeat to show that this worker is still alive.
 
 Run worker and wait for L</"WORKER SIGNALS">.
 
+  # Start a worker for a special named queue
+  my $worker = $minion->worker;
+  $worker->status->{queues} = ['important'];
+  $worker->run;
+
 These L</"status"> options are currently available:
 
 =over 2
