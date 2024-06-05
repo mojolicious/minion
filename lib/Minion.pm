00d25ae6bcd86e425221b4f57bec1f34ccd96dd3
Sebastian Riedel <s@kraih.com>
2024-06-05 19:31:14 +0200

    Bump version

--- lib/Minion.pm
+++ lib/Minion.pm
@@ -21,7 +21,7 @@ has missing_after                  => 1800;
 has [qw(remove_after stuck_after)] => 172800;
 has tasks                          => sub { {} };
 
-our $VERSION = '10.30';
+our $VERSION = '10.31';
 
 sub add_task {
   my ($self, $name, $task) = @_;
